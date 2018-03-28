0facdff2afd3e9bb2039400f6dc5e229f29e0a23
Matthew Boles <mboles@brightcove.com>
2018-03-28 16:39:47 -0400

    typo in comments

--- function-in-separate-file/disable-forward-scrubbing.js
+++ function-in-separate-file/disable-forward-scrubbing.js
@@ -1,12 +1,18 @@
+// +++ Define the middleware function+++
 var disableForwardScrubbbing = function(player) {
   return {
+    // +++ Set the source to use any tech +++
     setSource: function setSource(srcObj, next) {
       next(null, srcObj);
     },
+    // +++ Alter the setCurrentTime function +++
     setCurrentTime: function setCurrentTime(ct) {
+      // Check if the time scrubbed to is less than the current time
       if ( ct < player.currentTime() ) {
+        // If time scrubbed to less than current time go back to the spot
         return ct;
       } else {
+        // If time scrubbed to is past current time, leave playhead at current time
         return player.currentTime();
       }
     }
